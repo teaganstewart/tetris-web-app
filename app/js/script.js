7f4deb03b96cb8c7dcc0b9dce763a2c9951e1805
teaganstewart <65583615+teaganstewart@users.noreply.github.com>
2021-04-29 16:52:05 +1200

    Tetromino piece falling
    I have the movement of the tetromino piece working, including breakpoints. Need to get them to settle on the bottom of the board.

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,26 @@
+class Cell {
 
+    constructor(x, color, active) {
+        this.x = x;
+        this.color = color;
+        this.active = active;
+    }
+
+    updateColor(color) {
+        this.color = color;
+    }
+
+    setActive() {
+        this.active = true;
+    }
+
+    setInactive() {
+        this.active = false;
+    }
+
+}
+
+var boardArr = [];
 
 function initialiseBoard() {
 
@@ -7,6 +29,15 @@ function initialiseBoard() {
     for (let i = 0; i < 128; i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
+
+        if (i == 4) {
+            boardArr.push(new Cell(i, "red", true));
+        }
+        else {
+            boardArr.push(new Cell(i, "white", false));
+        }
+        
+        cell.id = "cell-" + i;
         board.appendChild(cell);
 
     }
@@ -23,10 +54,92 @@ function initialisePreview() {
     }
 }
 
+function runTetris() {
+    updateBlock();
+    
+}
+
+function updateBlock() {
+    for (var i = 0; i < 128; i++) {
+        if (boardArr[i].active) {
+            var nextX = boardArr[i].x + 8;
+            if (nextX >= 128) {
+                nextX = 4;
+            }
+            
+            moveCell(i, nextX);
+
+            break;
+        }
+    }
+}
+
+function moveCell(x, nextX) {
+    boardArr[x] = new Cell(x, "white", false);
+    var cell = document.getElementById("cell-" + x);
+    cell.classList.remove("red");
+    
+    boardArr[nextX] = new Cell(nextX, "red", true);
+    cell = document.getElementById("cell-" + nextX);
+    cell.classList.add("red");
+}
+
+function onKeyPress(event) {
+    switch (event.keyCode) {
+        case 37:
+            moveLeft();
+            break;
+        case 39:
+            moveRight();
+            break;
+        case 40:
+            updateBlock();
+            break;
+        default:
+            break;
+    }
+}
+
+function moveLeft() {
+    for (var i = 0; i < 128; i++) {
+        if (boardArr[i].active) {
+
+            var nextX = boardArr[i].x - 1;
+            if (i % 8 == 0) {
+                break;
+            }
+            
+            moveCell(i, nextX);
+
+            break;
+        }
+    }
+}
+
+function moveRight() {
+    for (var i = 0; i < 128; i++) {
+        if (boardArr[i].active) {
+
+            var nextX = boardArr[i].x + 1;
+            if (nextX  % 8 == 0) {
+                break;
+            }
+            
+            moveCell(i, nextX);
+
+            break;
+        }
+    }
+}
+
 ; (function () {
 
     initialiseBoard();
     initialisePreview();
 
+    window.addEventListener("keydown", onKeyPress);
+
+    setInterval(runTetris, 1000);
 })();
+
 
